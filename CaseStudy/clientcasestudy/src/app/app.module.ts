ed82c9bb8f6fd353f9e503cec5ab3ef3363b93dd
Evan <wilvann2012@gmail.com>
2024-09-29 02:27:37 -0400

    Lab3

---
+++ CaseStudy/clientcasestudy/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { MatComponentsModule } from './mat-components/mat-components.module';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { VendorModule } from './vendor/vendor.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatComponentsModule,
+    VendorModule,
+  ],
+  providers: [
+    provideClientHydration(),
+    provideAnimationsAsync(),
+    provideHttpClient(withFetch())
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule { }
---
+++ CaseStudy/clientcasestudy/src/app/vendor/vendor.ts
@@ -0,0 +1,14 @@
+export interface Vendor {
+  /**
+  * vendors - interface for an vendors entity
+  */
+  id: number;
+  name: string;
+  address1: string;
+  city: string;
+  province: string;
+  postalcode: string;
+  phone: string;
+  type: string;
+  email: string;
+}
