11ecbdf9fd8481921d165956021057aab1ea7cce
Evan <wilvann2012@gmail.com>
2024-09-29 02:23:36 -0400

    Lab2

---
+++ MavenProject/clientexercises/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { Location } from '@angular/common'
+
+@Component({
+  selector: 'app-exercises',
+  templateUrl: './app.component.html',
+  styles: ''
+})
+export class AppComponent {
+  title: string = '';
+  constructor(private location: Location) {
+    let path = location.path();
+    if (path && path.length > 1) {
+      let header = path.substring(1, 2).toUpperCase();
+      header += path.substring(2);
+      this.setTitle(header);
+    }
+    else if (path === '') {
+      this.setTitle('');
+    }
+  }
+  setTitle(header: string) {
+    this.title = header ? header : 'Home';
+  }
+}
---
+++ MavenProject/clientexercises/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// added imports
+import { MatComponentsModule } from './mat-components/mat-components.module';
+import { HomeComponent } from './home/home.component';
+
+import { EmployeeModule } from './employee/employee.module';
+
+
+@NgModule({
+declarations: [AppComponent, HomeComponent],
+imports: [
+BrowserModule,
+AppRoutingModule,
+BrowserAnimationsModule,
+MatComponentsModule,
+EmployeeModule
+],
+providers: [provideHttpClient(withFetch())],
+bootstrap: [AppComponent],
+})
+export class AppModule {}
